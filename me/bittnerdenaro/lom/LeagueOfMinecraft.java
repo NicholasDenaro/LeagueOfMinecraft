8a444fbca0d07dd376e62876bad027e3f4dcd8d0
Nicholas Denaro <NicholasCDenaro@aol.com>
2016-04-09 14:02:20 -0400

    Load handlers

--- me/bittnerdenaro/lom/LeagueOfMinecraft.java
+++ me/bittnerdenaro/lom/LeagueOfMinecraft.java
@@ -1,8 +1,25 @@
 package me.bittnerdenaro.lom;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class LeagueOfMinecraft extends JavaPlugin
 {
-
+	@Override
+	public void onEnable()
+	{
+		loadHandlers();
+		getServer().broadcastMessage(ChatColor.GREEN + "LeagueOfMinecraft enabled!");
+	}
+	
+	private void loadHandlers()
+	{
+		this.getServer().getPluginManager().registerEvents(new GeneralPlayerEvents(), this);
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		getServer().broadcastMessage(ChatColor.RED + "LeagueOfMinecraft disabled!");
+	}
 }
